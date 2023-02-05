4aeaab6ee1d0f4f31ce6b7e2a349c87e63410084
Fathima <fathimaas91@gmail.com>
2023-02-05 13:17:17 +0100

    added relavant icon

--- src/index.js
+++ src/index.js
@@ -25,13 +25,13 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = Math.round(response.data.name);
@@ -40,11 +40,13 @@ function displayTemperature(response) {
   );
   humidityElement.innerHTML = Math.round(response.data.main.humidity);
   windElement.innerHTML = Math.round(response.data.main.wind);
-  date;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src"`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-
 let apiKey = "f79c79fdb0dfd761d6485bc96743fedf";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units = metric`;
-
+let city = "Madrid";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units = metric`;
 axios.get(apiUrl).then(displayTemperature);
