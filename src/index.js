1dc5dd6acc08f2ca0ce9e826371ff96afcbd9509
Fathima <fathimaas91@gmail.com>
2023-02-05 15:13:58 +0100

    updated coordinates

--- src/index.js
+++ src/index.js
@@ -52,6 +52,11 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
 }
+function getForecast (coordinates)
+ let apiKey = "f79c79fdb0dfd761d6485bc96743fedf";
+ let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -75,6 +80,8 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+
+getForecast(response.data.coord)
 
 function search(city) {
   let apiKey = "f79c79fdb0dfd761d6485bc96743fedf";
